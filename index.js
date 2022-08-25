98085ac8f616a9208c2a3908a7facca9af853e7f
aasem-test-drive <111973087+aasem-test-drive@users.noreply.github.com>
2022-08-25 09:51:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var express = require('express');
-var app = express();
+const http = require('http')
+const fs = require('fs')
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html');
-});
+const port = process.env.PORT || 3000
+
+http.createServer((req, res) => {
+  const filePath = __dirname + (req.url === '/' ? '/index.html' : req.url)
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404)
+      res.end(JSON.stringify(err))
+      return
+    }
+    res.writeHead(200)
+    res.end(data)
+  })
+}).listen(port)
